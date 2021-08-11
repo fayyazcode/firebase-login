f4f7c11d9fc44cebdf00908b85717842044952b7
Fayyaz Alam <fayazalam.98@gmail.com>
2021-08-11 12:30:50 -0700

    firebase login added

--- login.js
+++ login.js
@@ -3,5 +3,16 @@ function gotohome() {
     var email = document.getElementById("inputEmail")
     var password = document.getElementById("inputPassword")
 
-
+    firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+        .then((userCredential) => {
+            // Signed in
+            var user = userCredential.user;
+            console.log("login ho gya",userCredential.user)
+            // ...
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorMessage)
+        });
 }
